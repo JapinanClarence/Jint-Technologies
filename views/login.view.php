5feb7319b8f215fe37b9d1441e8f90b56d739bd1
JapinanClarence <japinanclarence@gmail.com>
2022-12-19 21:04:39 +0800

    Updated the login form

--- views/login.view.php
+++ views/login.view.php
@@ -1,8 +1,32 @@
 <?php
 include "partials/header.php";
-include "partials/banner.php";
-?>
-
+// include "partials/banner.php";
+// ?>
+<div class="flex container login-page">
+    
+    <div class="container section">
+        <p>
+            Jint.Tech, offers the best services you ever need.
+        </p>
+    </div>
+    <div class="container section">
+        <form class="flex login-form" action="">
+            <div>
+                <label for="username">Username</label>
+                <input class="form-input" type="text">
+                <label for="password">Password</label>
+                <input class="form-input" type="password">
+            </div>
+            <div>
+                <button class="btn form-btn" type="submit">
+                    Login
+                </button>
+                <span> or Sign up</span>
+            </div>
+        </form>
+    
+    </div>
+</div>
     
 
 <?php
